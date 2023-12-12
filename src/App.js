2391c9d8f9dd17c595e97e0e72c7e9d743ed7277
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-12-12 20:42:35 +0700

    Mounted/Unmounted

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import { useState } from "react";
+import Content from "./Content";
 
 function App() {
-    const [job, setJob] = useState("");
-    const [jobs, setJobs] = useState(() => {
-        const storageJobs = JSON.parse(localStorage.getItem("jobs"));
-
-        return storageJobs ?? [];
-    });
-
-    const handleSubmit = () => {
-        setJobs((pre) => {
-            const newJobs = [...pre, job];
-
-            // Save to localStorage
-            const jsonJob = JSON.stringify(newJobs);
-            localStorage.setItem("jobs", jsonJob);
-
-            return newJobs;
-        });
-
-        setJob("");
-    };
+    const [show, setShow] = useState(false);
 
     return (
         <div style={{ padding: 32 }}>
-            <input value={job} onChange={(e) => setJob(e.target.value)} />
-            <button onClick={handleSubmit}>Add</button>
-            <ul>
-                {jobs.map((job, index) => (
-                    <li key={index}>{job}</li>
-                ))}
-            </ul>
+            {/* Mounted / Unmounted */}
+            <button
+                onClick={() => {
+                    setShow(!show);
+                }}
+            >
+                Toogle
+            </button>
+            {show && <Content />}
         </div>
     );
 }
---
+++ src/Content.js
@@ -0,0 +1,5 @@
+function Content() {
+    return <h1>Hello anh em anh em</h1>;
+}
+
+export default Content;
