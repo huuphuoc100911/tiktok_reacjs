a7a7ddddc4c769d5f2da393cf9c8c536c8650f7f
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-12-14 22:11:01 +0700

    react.memo

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
+import Content from "./Content";
 
 function App() {
-    const [count, setCount] = useState(60);
-
-    const timer = useRef();
-    const preCount = useRef();
-    const h1Ref = useRef();
-
-    useEffect(() => {
-        preCount.current = count;
-    }, [count]);
-
-    useEffect(() => {
-        console.log(h1Ref.current.getBoundingClientRect());
-    });
-
-    const handleStart = () => {
-        timer.current = setInterval(() => {
-            setCount((preCount) => preCount - 1);
-        }, 1000);
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(10);
+    const increase = () => {
+        setCount(count + 1);
     };
-
-    const handleStop = () => {
-        clearInterval(timer.current);
+    const increase2 = () => {
+        setCount2(count2 + 1);
     };
-
-    console.log(count, preCount.current);
 
     return (
         <div style={{ padding: 20 }}>
-            <h1 ref={h1Ref}>{count}</h1>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handleStop}>Stop</button>
+            <Content count2={count2} />
+            <h1>{count}</h1>
+            <button onClick={increase}>Increase</button>
+            <button onClick={increase2}>Increase2</button>
         </div>
     );
 }
