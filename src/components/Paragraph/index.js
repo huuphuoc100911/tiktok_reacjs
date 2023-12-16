1240efd00db296a459fc4a2174f869d175dfdb82
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-12-16 11:59:52 +0700

    Revert "CSS module"
    This reverts commit 5298cf0dbc9784413f3984483c8985f8db016dbe.

--- src/components/Paragraph/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "./Paragraph.module.css";
-
-function Paragraph() {
-    return (
-        <p className={styles.heading}>Waiting for update signal from WDS...</p>
-    );
-}
-
-export default Paragraph;
